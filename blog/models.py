99c8f964fbbe34ed2f1150a2820880dfaa6a4cbc
StefFrustagli <stefania.frustagli@outlook.it>
2024-02-21 07:33:13 +0000

    Add summernote package to improve admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
-from typing import Any
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -18,25 +17,23 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
-    class Meta:
-        ordering = ["-created_on", "author"]
-
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"        
-    )
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"Comment {self.body} by {self.author}"
     class Meta:
         ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
