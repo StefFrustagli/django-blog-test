5e6a55fb92e86759145c853ffac6233395f72b1e
StefFrustagli <stefania.frustagli@outlook.it>
2024-02-24 19:47:14 +0000

    Add post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = [
     "8000-ajgreaves-blog-lesson-pl-mjfxnpd4ht.us2.codeanyapp.com",
     ".herokuapp.com",
