8a38e25db8ed970524d18bc060891ad27590bcdf
StefFrustagli <stefania.frustagli@outlook.it>
2024-02-27 08:02:58 +0000

    Add count feature to comments

--- blog/models.py
+++ blog/models.py
@@ -26,6 +26,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter", default=1)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -34,4 +35,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.author}"
